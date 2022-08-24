56822576c37613d7125a88c1b189f97c4a196410
Devendra Yadav <devendra.yadav@gmail.com>
2022-08-25 03:04:49 +0800

    updated token functionality

--- contracts/MayudToken.sol
+++ contracts/MayudToken.sol
@@ -5,15 +5,74 @@ pragma solidity 0.8.9;
 import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 import "@openzeppelin/contracts/token/ERC20/extensions/ERC20Burnable.sol";
 import "@openzeppelin/contracts/access/Ownable.sol";
+import "hardhat/console.sol";
 
 contract MayudToken is ERC20, ERC20Burnable, Ownable{
 
+    address public marketingWallet;
+    address public developmentWallet;
+    uint8 private marketingFeePercentage;
+    uint8 private developmentFeePercentage;
+
+
     constructor() ERC20("Mayud", "MYD") {
-        _mint(msg.sender, 1000000000 * 10 ** decimals());
+        _mint(msg.sender, 100000000000 * 10 ** decimals());
+
+        //Default values
+        marketingFeePercentage = 2;
+        developmentFeePercentage = 2;
+
     }
 
+    /**
+     * @dev This function will mint new tokens
+     * @param to the address where the newly minted tokens will be sent.
+     * @param amount the amount of tokens to mint.
+     */
     function mint(address to, uint256 amount) public onlyOwner {
+        amount = amount * 10 ** decimals();
         _mint(to, amount);
+    }
+
+    function transfer(address to, uint256 amount) public override returns (bool) {
+        
+         if(_msgSender() != owner()){
+            uint256 marketingFee = (marketingFeePercentage * amount) / 100;
+            uint256 developmentFee = (developmentFeePercentage * amount) / 100;
+
+            amount = amount - (marketingFee + developmentFee);
+
+            super.transfer(marketingWallet, marketingFee);
+            super.transfer(developmentWallet, developmentFee);
+        
+        }
+
+        super.transfer(to, amount);
+
+        return true;
+    }
+
+
+    function setMarketingWallet(address walletAddress) public onlyOwner{
+
+        require(walletAddress != address(0), "Wallet address cant be 0 address");
+        marketingWallet = walletAddress;
+    }
+    
+    function setDevelopmentWallet(address walletAddress) public onlyOwner{
+
+        require(walletAddress != address(0), "Wallet address cant be 0 address");
+        developmentWallet = walletAddress;
+    }
+
+    function setMarketingFeePercentage(uint8 value) public onlyOwner{
+        require(value >0 && value <100, "Value must be between 0 and 100");
+        marketingFeePercentage = value;
+    }
+
+    function setDevelopmentFeePercentage(uint8 value) public onlyOwner{
+        require(value >0 && value <100, "Value must be between 0 and 100");
+        developmentFeePercentage = value;
     }
 
 }
--- test/mayudToken.js
+++ test/mayudToken.js
@@ -3,13 +3,22 @@ const {loadFixture} = require('@nomicfoundation/hardhat-network-helpers');
 const {ethers} = require('hardhat');
 const {expect} = require('chai');
 
+let humanReadableTokenAmount = (n) => {
+    return ethers.utils.parseEther(n.toString());
+}
+
+let formatEther = (n) => {
+    return ethers.utils.formatEther(n.toString());
+
+}
+
 describe('MayudToken', () => {
     async function contractDeployment(){
-        const [deployer] = await ethers.getSigners();
+        const [deployer, holder1, holder2] = await ethers.getSigners();
         let MayudToken = await ethers.getContractFactory('MayudToken');
         let mayudToken = await MayudToken.deploy();
         await mayudToken.deployed();
-        return{deployer, mayudToken};
+        return{deployer, mayudToken, holder1, holder2};
     }    
 
     describe("Token Deployment", ()=>{
@@ -17,6 +26,71 @@ describe('MayudToken', () => {
             const {deployer, mayudToken} = await loadFixture(contractDeployment);
             console.log(`MayudToken deployed at: ${mayudToken.address}`);
             expect(mayudToken.address).to.be.properAddress;
+        })
+    })
+
+    describe("Token functionality", () => {
+        it("should have initial supply of 1 billion tokens", async () => {
+            const {deployer, mayudToken} = await loadFixture(contractDeployment);
+
+            let initialSupply = formatEther(await mayudToken.balanceOf(deployer.address));
+            console.log(`Balance of deployer ${initialSupply}`);
+            expect(initialSupply).to.be.equal('100000000000.0');
+        })
+
+        it("should be able to mint new tokens", async ()=>{
+            const {deployer, mayudToken} = await loadFixture(contractDeployment);
+
+            await mayudToken.mint(deployer.address, 100000);
+
+            let currentSupply = formatEther(await mayudToken.balanceOf(deployer.address));
+            console.log(`Current Supply ${currentSupply}`);
+            expect(currentSupply).to.be.equal('100000100000.0');
+        })
+
+        it("should be able to transfer tokens from deployer to holders", async ()=> {
+            const {deployer, mayudToken, holder1, holder2} = await loadFixture(contractDeployment);
+            let deployerBalance = formatEther(await mayudToken.balanceOf(deployer.address));
+            console.log(`Deployer Balance Before : ${deployerBalance}`);
+
+            await mayudToken.transfer(holder1.address, humanReadableTokenAmount(1000000));
+
+            let holder1Balance = formatEther(await mayudToken.balanceOf(holder1.address));
+            console.log(`Holder1 Balance : ${holder1Balance}`);
+
+            deployerBalance = formatEther(await mayudToken.balanceOf(deployer.address));
+            console.log(`Deployer Balance after : ${deployerBalance}`);
+
+            expect(deployerBalance).to.equal('99999000000.0');
+            expect(holder1Balance).to.equal('1000000.0');
+
+        })
+
+        it("should be able to transfer tokens from holder1 to holder2", async ()=> {
+            const {deployer, mayudToken, holder1, holder2} = await loadFixture(contractDeployment);
+            let deployerBalance = formatEther(await mayudToken.balanceOf(deployer.address));
+            console.log(`Deployer Balance Before : ${deployerBalance}`);
+
+            //Deployer to send some balance to holder1 so that he can transfer.
+            await mayudToken.transfer(holder1.address, humanReadableTokenAmount(1000000));
+
+
+            //Holder1 to transfert to holder2
+            await mayudToken.connect(holder1).transfer(holder2.address, humanReadableTokenAmount(50000));
+
+            let holder1Balance = formatEther(await mayudToken.balanceOf(holder1.address));
+            console.log(`Holder1 Balance : ${holder1Balance}`);
+
+            let holder2Balance = formatEther(await mayudToken.balanceOf(holder2.address));
+            console.log(`Holder2 Balance : ${holder2Balance}`);
+
+            deployerBalance = formatEther(await mayudToken.balanceOf(deployer.address));
+            console.log(`Deployer Balance after : ${deployerBalance}`);
+
+            expect(deployerBalance).to.equal('99999000000.0');
+            expect(holder1Balance).to.equal('950000.0');
+            expect(holder2Balance).to.equal('50000.0');
+
         })
     })
 })
